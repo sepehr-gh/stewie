029abf529ca16d6a82f309f757daae17826639c6
sepehr-gh <3pehr.gh992@gmail.com>
2016-01-18 22:09:33 +0330

    README added - USAGE / EXAMPLES

--- core/defines/packages.php
+++ core/defines/packages.php
@@ -3,4 +3,5 @@
 $stewie_packages = array(
     "PdoWrapper" => _core_ . "/database/pdo_wrapper/class.pdowrapper.php",
     "ParsePath" => _core_ . "/lib/parse_path.php",
+    "SmartStewie" => _core_ . "/template_engine/SmartStewie.php",
 );
